047c19bd351673115312b8c009e0af3fcaf12147
Brian <br.skrzynka@gmail.com>
2019-12-21 12:18:55 +0100

    make an inout from user and basic mathematical operation

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -1,14 +1,25 @@
 package machine;
 
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
 public class CoffeeMachine {
     public static void main(String[] args) {
-        System.out.println("Starting to make a coffee");
-        System.out.println("Grinding coffee beans");
-        System.out.println("Boiling water");
-        System.out.println("Mixing boiled water with crushed coffee beans");
-        System.out.println("Pouring coffee into the cup");
-        System.out.println("Pouring some milk into the cup");
-        System.out.println("Coffee is ready!");
+
+        Scanner scanner = new Scanner(in);
+
+        System.out.println("Write how many cups of coffee you will need: ");
+
+        String input = scanner.nextLine();
+        int cups = Integer.parseInt(input);
+        int water = cups*200;
+        int milk = cups* 50;
+        int coffeBeans= cups*15;
+        System.out.printf("For %s cups of coffee you will need:\n",input);
+        System.out.printf("%d ml of water\n",water);
+        System.out.printf("%d ml of milk\n",milk);
+        System.out.printf("%d g of coffee beans\n",coffeBeans);
 
     }
 }
