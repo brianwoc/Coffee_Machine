811970b78bbc8eccb245d6095cd44f037f7d20fd
Brian <br.skrzynka@gmail.com>
2019-12-21 15:46:19 +0100

    add functionality to check how many coffes machine can do with initial amount of ingredients

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -1,25 +1,57 @@
 package machine;
 
-import java.util.Scanner;
+import java.util.*;
 
 import static java.lang.System.in;
+import static java.lang.System.setOut;
 
 public class CoffeeMachine {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(in);
 
-        System.out.println("Write how many cups of coffee you will need: ");
+        System.out.println("Write how many ml of water the coffee machine has:");
+        int water = scanner.nextInt();
 
-        String input = scanner.nextLine();
-        int cups = Integer.parseInt(input);
-        int water = cups*200;
-        int milk = cups* 50;
-        int coffeBeans= cups*15;
-        System.out.printf("For %s cups of coffee you will need:\n",input);
-        System.out.printf("%d ml of water\n",water);
-        System.out.printf("%d ml of milk\n",milk);
-        System.out.printf("%d g of coffee beans\n",coffeBeans);
+        System.out.println("Write how many ml of milk the coffee machine has:");
+        int milk = scanner.nextInt();
+
+        System.out.println("Write how many grams of coffee beans the coffee machine has:");
+        int coffeBeans = scanner.nextInt();
+
+        System.out.println("Write how many cups of coffee you will need: ");
+        int cups = scanner.nextInt();
+
+        if (water >= 200 * cups && milk >= 50 * cups && coffeBeans >= 15 * cups) {
+            if (water >= 200 * (cups + 1) && milk >= 50 * (cups + 1) && coffeBeans >= 15 * (cups + 1) ){
+
+                int one = water / 200;
+                int two = milk / 50;
+                int three = coffeBeans / 15;
+                List<Integer> list = new LinkedList<Integer>();
+                list.add(one);
+                list.add(two);
+                list.add(three);
+                int cupsTotal = Collections.min(list);
+                System.out.printf("Yes, I can make that amount of coffee (and even %d more than that)", cupsTotal -cups);
+            }else {
+                System.out.printf("Yes, I can make that amount of coffee");
+            }
+        }else {
+
+            int one = water / 200;
+            int two = milk / 50;
+            int three = coffeBeans / 15;
+            List<Integer> list = new LinkedList<Integer>();
+            list.add(one);
+            list.add(two);
+            list.add(three);
+            cups = Collections.min(list);
+            System.out.printf("No, I can make only %d cup(s) of coffee", cups);
+
+        }
+
+
 
     }
 }
