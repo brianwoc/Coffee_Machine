2eefe7861234ec9a4f850c0930fc9a36090c9505
Brian <br.skrzynka@gmail.com>
2019-12-21 21:14:19 +0100

    add new feature to get coffee, add supply and take money from the machine

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -10,48 +10,78 @@ public class CoffeeMachine {
 
         Scanner scanner = new Scanner(in);
 
-        System.out.println("Write how many ml of water the coffee machine has:");
-        int water = scanner.nextInt();
+        int water = 1200;
+        int milk = 540;
+        int coffeBeans = 120;
+        int disposalCups = 9;
+        int cash = 550;
+        getInfo(water, milk, coffeBeans, disposalCups, cash);
+        System.out.println("Write action (buy, fill, take):");
+        String action = scanner.nextLine();
 
-        System.out.println("Write how many ml of milk the coffee machine has:");
-        int milk = scanner.nextInt();
+        switch (action) {
+            case "buy":
+                System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:");
+                String buyProduct = scanner.nextLine();
+                switch (buyProduct) {
+                    case "1":
+                        water -= 250;
+                        coffeBeans -= 16;
+                        disposalCups--;
+                        cash += 4;
+                        getInfo(water, milk, coffeBeans, disposalCups, cash);
+                        break;
+                    case "2":
+                        water -= 350;
+                        milk -= 75;
+                        coffeBeans -= 20;
+                        disposalCups--;
+                        cash += 7;
+                        getInfo(water, milk, coffeBeans, disposalCups, cash);
+                        break;
+                    case "3":
+                        water -= 200;
+                        milk -= 100;
+                        coffeBeans -= 12;
+                        disposalCups--;
+                        cash += 6;
+                        getInfo(water, milk, coffeBeans, disposalCups, cash);
+                        break;
 
-        System.out.println("Write how many grams of coffee beans the coffee machine has:");
-        int coffeBeans = scanner.nextInt();
+                }
+                break;
+            case "fill":
+                System.out.println("Write how many ml of water do you want to add:");
+                water = scanner.nextInt()+water;
+                System.out.println("Write how many ml of milk do you want to add:");
+                milk = scanner.nextInt()+milk;
+                System.out.println("Write how many grams of coffee beans do you want to add:");
+                coffeBeans = scanner.nextInt()+coffeBeans;
+                System.out.println("Write how many disposable cups of coffee do you want to add:");
+                disposalCups = scanner.nextInt()+disposalCups;
+                getInfo(water, milk, coffeBeans, disposalCups, cash);
+                break;
+            case "take":
+                System.out.println("I gave you $"+cash);
+                cash=0;
+                getInfo(water,milk,coffeBeans,disposalCups,cash);
+                break;
 
-        System.out.println("Write how many cups of coffee you will need: ");
-        int cups = scanner.nextInt();
 
-        if (water >= 200 * cups && milk >= 50 * cups && coffeBeans >= 15 * cups) {
-            if (water >= 200 * (cups + 1) && milk >= 50 * (cups + 1) && coffeBeans >= 15 * (cups + 1) ){
-
-                int one = water / 200;
-                int two = milk / 50;
-                int three = coffeBeans / 15;
-                List<Integer> list = new LinkedList<Integer>();
-                list.add(one);
-                list.add(two);
-                list.add(three);
-                int cupsTotal = Collections.min(list);
-                System.out.printf("Yes, I can make that amount of coffee (and even %d more than that)", cupsTotal -cups);
-            }else {
-                System.out.printf("Yes, I can make that amount of coffee");
-            }
-        }else {
-
-            int one = water / 200;
-            int two = milk / 50;
-            int three = coffeBeans / 15;
-            List<Integer> list = new LinkedList<Integer>();
-            list.add(one);
-            list.add(two);
-            list.add(three);
-            cups = Collections.min(list);
-            System.out.printf("No, I can make only %d cup(s) of coffee", cups);
 
         }
 
 
 
     }
+
+    public static void getInfo(int water, int milk, int coffeBeans, int disposalCups, int cash) {
+        System.out.println("The coffee machine has:");
+        System.out.printf("%d of water\n", water);
+        System.out.printf("%d of milk\n", milk);
+        System.out.printf("%d of coffee beans\n", coffeBeans);
+        System.out.printf("%d of disposable cups\n", disposalCups);
+        System.out.printf("%d of money\n", cash);
+    }
 }
+
